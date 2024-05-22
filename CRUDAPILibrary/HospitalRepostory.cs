9d3144540f91666dcb7c782753da6f9456c86d91
RaghuKV <raghuvenkatasamy@gmail.com>
2024-05-22 14:22:59 +0530

    working for API

---
+++ CRUDAPILibrary/HospitalRepostory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CRUDAPILibrary
+{
+    public class HospitalRepostory
+    {
+        IConfiguration _configurestion;
+        public HospitalRepostory(IConfiguration configurestion)
+        {
+
+            _configurestion = configurestion;
+            var a = _configurestion.GetConnectionString("DbConnection");
+
+        }
+    }
+}
+
+
